510f9faee0888085b9c89675f6dcf2cb923ddba1
Eulentier161 <git@eule.wtf>
2023-11-30 14:20:00 +0100

    Enable tracemalloc to get the object allocation traceback

--- it2zbot/cogs/poll.py
+++ it2zbot/cogs/poll.py
@@ -89,7 +89,7 @@ class PollCog(commands.GroupCog, name="poll"):
         option10: Optional[str],
     ):
         if not interaction.guild:
-            return interaction.response.send_message("this command can only be used in guilds")
+            return await interaction.response.send_message("this command can only be used in guilds")
         options = [option1, option2, option3, option4, option5, option6, option7, option8, option9, option10]
         options: list[str] = [o for o in options if o is not None]
         options: list[Option] = [
