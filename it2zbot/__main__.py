c7066a0a4ec2f961fabe6869f85198388e96025e
Eulentier161 <git@eule.wtf>
2025-12-27 11:41:28 +0100

    gist previews

---
+++ it2zbot/__main__.py
@@ -0,0 +1,4 @@
+from it2zbot.bot import main
+
+if __name__ == "__main__":
+    main()
--- it2zbot/cogs/github.py
+++ it2zbot/cogs/github.py
@@ -1,7 +1,11 @@
+from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
 import discord
 import httpx
+import re
+from urllib.parse import urlparse
+from pydantic import BaseModel
 from discord import app_commands
 from discord.app_commands import locale_str
 from discord.ext import commands
@@ -62,3 +66,57 @@ class GithubCog(commands.GroupCog, name=locale_str("github")):
     )
     async def issue(self, interaction: discord.Interaction):
         await interaction.response.send_modal(Issue(interaction=interaction))
+
+    @commands.Cog.listener("on_message")
+    async def respond_to_gist_link(self, message: discord.Message):
+        if message.author.bot or message.author == self.bot.user:
+            return
+
+        gist_ids = re.findall(r"https://gist\.github\.com/\w+/(\w+)", message.content, re.MULTILINE)
+        if len(gist_ids) == 0:
+            return
+
+        class GistOwner(BaseModel):
+            login: str
+
+        class GistFile(BaseModel):
+            filename: str
+            content: str
+            size: int
+
+        class GistData(BaseModel):
+            files: dict[str, GistFile]
+            owner: GistOwner
+            html_url: str
+            description: str
+
+        def build_gist_embeds(gist_data: GistData) -> list[discord.Embed]:
+            MAX_CHARS_EMBEDS = 6000
+            MAX_CHARS_EMBED_DESCRIPTION = 4096
+
+            embeds: list[discord.Embed] = []
+            for filename in list(gist_data.files.keys())[:10]:
+                file = gist_data.files[filename]
+                MAX_EMBED_CHARS_BY_CNT_EMBEDS = MAX_CHARS_EMBEDS // min(10, len(gist_data.files))
+                cnt_lost_chars_from_wrapper = len(f"```{filename.split('.')[-1]}\n\n```")
+                max_description_length = min(
+                    MAX_CHARS_EMBED_DESCRIPTION - cnt_lost_chars_from_wrapper, MAX_EMBED_CHARS_BY_CNT_EMBEDS
+                )
+                embed = discord.Embed(
+                    title=file.filename,
+                    description=f"```{filename.split('.')[-1]}\n{file.content[:max_description_length]}\n```",
+                )
+                embeds.append(embed)
+            return embeds
+
+        async with httpx.AsyncClient() as client:
+            for gist_id in gist_ids:
+                res = await client.get(f"https://api.github.com/gists/{gist_id}")
+                if res.status_code != 200:
+                    continue
+                gist_data = GistData.model_validate(res.json())
+                embeds = build_gist_embeds(gist_data)
+                await message.reply(
+                    f"# [Gist by {gist_data.owner.login}](<{gist_data.html_url}>)\n-# {gist_data.description}",
+                    embeds=embeds,
+                )
